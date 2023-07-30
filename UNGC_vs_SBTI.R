b1a2ec6ce64baf2d47700c5647c1a4d0dbfdcbce
jpBane <padberg.joerg@gmail.com>
2023-07-30 23:52:02 +0200

    Created a first matrix with string distances

--- UNGC_vs_SBTI.R
+++ UNGC_vs_SBTI.R
@@ -1,14 +1,29 @@
+# ----------------
 # Load Packages
+# ----------------
 library(readxl)
 library(stringdist)
+library(dplyr)
+library(ggplot2)
 
-# Import the data
-download.file("https://sciencebasedtargets.org/download/excel", destfile = "SBTI_participants.xlsx")
-sbti <- read_excel("SBTI_participants.xlsx")
+# ----------------
+# Import data
+# ----------------
+#download.file("https://sciencebasedtargets.org/download/excel", destfile = "SBTI_participants.xlsx") # does not work?
+sbti <- read_excel("companies-taking-action.xlsx")
 
+# use the web_scraping.r script to get the data
+ungc <- readRDS("UNGC_participants.Rds")
 
-# Prepare the data
-...
+# -----------------------
+# Exploratory Analysis 
+# -----------------------
+summary(ungc)
+summary(sbti)
 
+dist_matrix <- stringdist(ungc$Name, sbti$`Company Name`)
+hist(dist_matrix)
+
+# ---------------------------
 # Perform the comparision
-stringdist(...)
+# ---------------------------
