9a0207baae90c962fab466534b6ffdef4f136a15
jpBane <padberg.joerg@gmail.com>
2023-08-03 00:03:16 +0200

    Added code for the comparison of the participants.

--- UNGC_vs_SBTI.R
+++ UNGC_vs_SBTI.R
@@ -27,3 +27,39 @@ hist(dist_matrix)
 # ---------------------------
 # Perform the comparision
 # ---------------------------
+find_similar_strings <- function(vector1, vector2, threshold = 2) {
+  # Initialize an empty list to store similar strings
+  similar_strings <- list()
+  
+  # Loop through the first vector
+  for (str1 in vector1) {
+    # Initialize a list to store similar strings for the current element in vector1
+    similar_to_str1 <- list()
+    
+    # Loop through the second vector
+    for (str2 in vector2) {
+      # Calculate the Levenshtein distance between str1 and str2
+      distance <- stringdist::stringdist(str1, str2, method = "lv")
+      
+      # If the Levenshtein distance is below the threshold, consider the strings similar
+      if (distance <= threshold) {
+        similar_to_str1 <- c(similar_to_str1, str2)
+      }
+    }
+    
+    # Add the list of similar strings for str1 to the main list
+    similar_strings[[str1]] <- similar_to_str1
+  }
+
+  # Return the list of similar strings
+  return(similar_strings)
+}
+
+# Perform a test with a smaller vector
+vector1 <- ungc$Name[1:1000]
+vector2 <- sbti$`Company Name`[1:1000]
+
+test <- find_similar_strings(vector1, vector2)
+
+# Filter the relevant information from the list
+test_filtered <- test[lengths(test) > 0]
